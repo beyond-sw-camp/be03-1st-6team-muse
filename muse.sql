a1489640284c839d9931dd89d7e12ad7c953cba3
sinaetown <123349044+sinaetown@users.noreply.github.com>
2023-12-01 15:05:13 +0900

    SQL DDL 작성
     테이블 :
    'comment'
    'follow'
    'image'
    'likes'
    'members'
    'music'
    'playlist'
    'playlist_music'

---
+++ muse.sql
@@ -0,0 +1,107 @@
+create database muse;
+use muse;
+
+CREATE TABLE members(
+  members_id bigint PRIMARY KEY AUTO_INCREMENT,
+  email varchar(100) UNIQUE NOT NULL,
+  password varchar(255) NOT NULL,
+  name varchar(200) NOT NULL,
+  nickname varchar(200) NOT NULL,
+  gender ENUM ('M', 'F'),
+  phone_number varchar(50) UNIQUE NOT NULL,
+  birth_date datetime NOT NULL,
+  created_at datetime DEFAULT now(),
+  updated_at datetime,
+  deleted_at datetime,
+  deleted_YN tinyint(1) DEFAULT 0
+);
+
+CREATE TABLE playlist(
+  playlist_id bigint PRIMARY KEY AUTO_INCREMENT,
+  playlist_name varchar(100) NOT NULL,
+  created_at datetime DEFAULT now(),
+  updated_at datetime,
+  deleted_at datetime,
+  deleted_YN tinyint(1) DEFAULT 0,
+  members_id bigint,
+FOREIGN KEY (members_id) REFERENCES members (members_id)
+);
+
+CREATE TABLE music(
+  music_id bigint PRIMARY KEY AUTO_INCREMENT,
+  title varchar(100) NOT NULL,
+  artist varchar(200) NOT NULL,
+  description varchar(3000),
+  music_file_url varchar(255) NOT NULL,
+  genre ENUM ('Kpop', 'Jpop', 'OST', 'Pop', 'Classic', 'Jazz', 'Indie', 'Canadian_blues', 
+  'Contemporary_RnB', 'Punk_blues', 'Crossover_music', 'Instrumental', 'Lofi', 'Blues_rock', 
+  'Chicago_blues', 'Delta_blues', 'EDM', 'Country_pop', 'Country_rap', 'Country_rock', 'Instrumental_country', 
+  'Western', 'New_age', 'Darkcore', 'Disco', 'City_pop', 'Dance_pop', 'Electronic_rock', 'Dance_rock', 
+  'Alternative_dance', 'Madchester', 'Baggy', 'New_rave', 'New_romantic', 'Electropop', 
+  'Electronica', 'Folktronica', 'Acid_jazz','Jungle', 'Hipster_hop', 'Cloud_rap', 'Acid_house', 
+  'Chicago_house', 'Futurepop', 'Dubstep'),
+  on_off tinyint(1),
+  created_at datetime DEFAULT now(),
+  updated_at datetime,
+  deleted_at datetime,
+  deleted_YN tinyint(1) DEFAULT 0,
+  members_id bigint,
+	FOREIGN KEY(members_id) REFERENCES members(members_id)
+);
+
+CREATE TABLE follow(
+  follow_id bigint PRIMARY KEY AUTO_INCREMENT,
+  created_at datetime DEFAULT now(),
+  unfollow_at datetime,
+  unfollow_YN tinyint(1) DEFAULT 0,
+  follow_ing bigint,
+  follower bigint,
+	FOREIGN KEY (follow_ing) REFERENCES members (members_id),
+    FOREIGN KEY (follower) REFERENCES members (members_id)
+    );
+    
+CREATE TABLE likes(
+  likes_id bigint PRIMARY KEY AUTO_INCREMENT,
+  created_at datetime DEFAULT now(),
+  canceled_at datetime,
+  canceled_YN tinyint(1) DEFAULT 0,
+  type ENUM ('members', 'music', 'playlist') NOT NULL,
+  members_id bigint,
+  identifier_id bigint,
+  FOREIGN KEY (members_id) REFERENCES members (members_id),
+FOREIGN KEY (identifier_id) REFERENCES playlist (playlist_id),
+FOREIGN KEY (identifier_id) REFERENCES members (members_id),
+FOREIGN KEY (identifier_id) REFERENCES music (music_id)
+);
+
+CREATE TABLE playlist_music(
+  pm_id bigint PRIMARY KEY AUTO_INCREMENT,
+  created_at datetime DEFAULT now(),
+  updated_at datetime,
+  deleted_at datetime,
+  deleted_YN tinyint(1) DEFAULT 0,
+  playlist_id bigint,
+  music_id bigint,
+ FOREIGN KEY (playlist_id) REFERENCES playlist (playlist_id),
+ FOREIGN KEY (music_id) REFERENCES music (music_id));
+ 
+CREATE TABLE image(
+  image_id bigint PRIMARY KEY AUTO_INCREMENT,
+  type_ ENUM ('members', 'music', 'playlist') NOT NULL,
+  image_url varchar(255),
+  identifier_id bigint,
+FOREIGN KEY (identifier_id) REFERENCES playlist (playlist_id),
+FOREIGN KEY (identifier_id) REFERENCES members (members_id),
+FOREIGN KEY (identifier_id) REFERENCES music (music_id));
+
+CREATE TABLE comment(
+  comment_id bigint PRIMARY KEY AUTO_INCREMENT,
+  comment_chk tinyint(1),
+  contents varchar(300),
+  created_at datetime DEFAULT now(),
+  updated_at datetime,
+  deleted_at datetime,
+  members_id bigint,
+  music_id bigint,
+FOREIGN KEY (music_id) REFERENCES music (music_id),
+FOREIGN KEY (members_id) REFERENCES members (members_id));
