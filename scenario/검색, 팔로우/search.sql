4608a92c6635ec1207ee4105ec8e20a2bb5a1f51
GyeongNam <jo_gn97@naver.com>
2023-12-02 21:28:48 +0900

    플레이리스트 검색

--- scenario/검색, 팔로우/search.sql
+++ scenario/검색, 팔로우/search.sql
@@ -59,5 +59,38 @@ and m.deleted_YN = 0 and m.on_off = 1
 group by m.music_id
 order by like_count desc;
 
--- 플레이리스트 검색 : 플레이리스트 제목, 소유자 이름,닉네임으로 검색할 수 있음. 정렬 기준은 좋아요 순을 기본으로 함.
+-- 플레이리스트 검색 : 플레이리스트 제목, 소유자 이름, 닉네임으로 검색할 수 있음. 정렬 기준은 좋아요 순을 기본으로 함.
+
+-- 테스트 전 상황
+select 
+p.playlist_id,
+p.playlist_name,
+m.name,
+m.nickname,
+m.deleted_YN as user_deleted_YN,
+p.deleted_YN as playlist_deleted_YN,
+p.on_off as playlist_on_off
+from members as m
+join playlist as p
+on m.members_id = p.members_id
+left join likes as l
+on p.playlist_id = l.playlist_id;
+
+-- 플레이리스트 검색 : a
+select 
+p.playlist_id,
+p.playlist_name,
+m.name,
+m.nickname,
+m.deleted_YN as user_deleted_YN,
+p.deleted_YN as playlist_deleted_YN,
+p.on_off as playlist_on_off
+from members as m
+join playlist as p
+on m.members_id = p.members_id
+left join likes as l
+on p.playlist_id = l.playlist_id
+where (p.playlist_name like '%a%' or m.name like '%a%' or m.nickname like '%d%') 
+and m.deleted_YN = 0 and p.deleted_YN = 0 and  p.on_off = 1
+group by p.playlist_id
 
